4975eef460f94353ba3997b896ec2bce68efbe28
Anushka Kawalkar <anushkakawalkar18@gmail.com>
2026-08-24 21:44:16 +0530

    Update project

--- frontend/src/components/AchievementsSummaryCard.jsx
+++ frontend/src/components/AchievementsSummaryCard.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+  import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Trophy } from 'lucide-react'
 import { listAchievements } from '../api/achievements'
